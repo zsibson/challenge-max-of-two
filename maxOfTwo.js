2dc3b6c19a1e3c7c161700f532a4ca2fda4f1253
zachary sibson <zac.sibson@gmail.com>
2022-07-07 08:01:40 -0600

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,14 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if(x > y){
+    console.log('X is greater.');
+    } 
+  else if (y > x){
+    console.log('Y is greater');
+  }
+  else {
+    console.log('Values are Identical');
+  }
